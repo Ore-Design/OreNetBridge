fcc9afca19098662d85d3032f28427a0548ecc46
Jacob Batista <jacobaccio@gmail.com>
2025-01-28 09:17:30 -0700

    flORE Stuff

--- src/main/java/design/ore/api/orenetbridge/packets/QueriedLineItem.java
+++ src/main/java/design/ore/api/orenetbridge/packets/QueriedLineItem.java
@@ -16,5 +16,5 @@ import lombok.Setter;
 @NoArgsConstructor
 public class QueriedLineItem
 {
-	String name, buildUID, description, buildRecordId, buildUid;
+	String name, buildUID, description, buildRecordId, buildUid, workOrders;
 }
